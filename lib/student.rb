ed1c8b9789110f9099cb42c3859b3fa23bbd6dce
Kevin Chi <kevinchi118@gmail.com>
2018-07-17 01:35:55 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
             INSERT INTO students (name, grade)
             VALUES (?, ?)
         SQL
-        DB[:conn].execute(sql, name:, grade:)
+        DB[:conn].execute(sql, name, grade)
     end
 
 
