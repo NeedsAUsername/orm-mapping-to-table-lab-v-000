576363d26cd037a2257673965828b49c609fb7a4
Kevin Chi <kevinchi118@gmail.com>
2018-07-17 01:23:21 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,18 @@ class Student
     def initialize(name, grade)
         @name = name
         @grade = grade
-    end 
+    end
+
+    def create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        SQL
+        DB[:conn].execute(sql)
+    end
 
 
 
